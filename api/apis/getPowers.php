1fe6680fa92d35b71d6c2ce0b169c7c8dddee6c6
sukhesh-vsk <sukhesh.vsk2005@gmail.com>
2024-07-11 01:37:46 +0530

    trying to call variables, methods from other file

---
+++ api/apis/getPowers.php
@@ -0,0 +1,11 @@
+ <?php
+
+class SuperHero {
+    private $power = "Fight against villans";
+
+    public function getPowers() {
+        return $this->power;
+    }
+}
+
+$user = "batman";
--- magic.php
+++ magic.php
@@ -1,12 +1,51 @@
+<pre>
 <?php
 
-class MagicMethods {
-    public function getName() {
-        echo "hello";
+class Temp {
+    private $frnd = "John";
+
+    public function getFrndName() {
+        echo "Temp Frnd Name :  $this->frnd" . "\n";
     }
 }
 
-echo "hello";
+
+class MagicMethods {
+    private $frnd = "Azar";
+    public function getName() {
+        echo "hello" . "\n";
+    }
+
+    private function getFrndName() {
+        echo "Frnd Name :  $this->frnd" . "\n";
+    }
+
+    public function __call($method, $args) {
+        if(method_exists(new Temp(), $method)) {
+            call_user_func_array([new Temp(), $method], $args);
+            return;
+        } 
+
+        $methods = scandir(__DIR__ . '/api/apis');
+
+        foreach($methods as $m) {
+            if($m == '.' || $m == '..') {
+                continue;
+            } 
+            $basem = basename($m, '.php');
+            require_once(__DIR__ . '/api/apis/' . $m);
+            echo $user; 
+            
+        }
+    }
+}
+
 $obj = new MagicMethods();
 $obj->getName();
+$obj->getFrndName();
+$obj->helloGuys();
+$hero = new SuperHero();
+echo $hero->getPowers();
 
+?>
+</pre>
